d27c088f0b28f419190564dd93f020bc24d326ca
Joel Kambulow <j2kambul@uwaterloo.ca>
2025-02-07 20:23:08 -0500

    fixed typo

--- install_nvim.sh
+++ install_nvim.sh
@@ -5,7 +5,7 @@ sudo apt remove neovim -y
 mkdir -p ~/.config/nvim
 curl -sL https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz \
 | sudo tar -xzf - --strip-components=1 --overwrite -C /usr
-wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz j
+wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 cat nvimrc > ~/.config/nvim/init.vim
